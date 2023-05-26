9d264fca6918a1aefe01400a57b6a0a5a05693cd
Nazar Antoniuk <antonuk.nazar09@gmail.com>
2023-05-26 06:38:08 +0300

    feat: customizes VuePress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+import { defaultTheme, defineUserConfig } from 'vuepress';
+import * as config from './configs/index';
+
+export default defineUserConfig({
+  shouldPrefetch: true,
+  locales: {
+    ...config.siteEn,
+  },
+  theme: defaultTheme({
+    logo: '/images/metarhia.png',
+    locales: {
+      ...config.localeEn,
+    },
+    repo: 'https://github.com/metarhia',
+    docsRepo: 'https://github.com/Nazar-Ant/metarhia-docs',
+    docsDir: 'docs',
+    docsBranch: 'main',
+    editLink: true,
+    editLinkPattern: ':repo/edit/:branch/:path',
+    repoLabel: 'GitHub',
+  }),
+});
---
+++ docs/.vuepress/configs/en.js
@@ -0,0 +1,96 @@
+export const siteEn = {
+  '/': {
+    lang: 'English',
+    title: 'Metarhia',
+    description: 'Enterprise technology stack for Node.js private clouds',
+  },
+};
+
+export const localeEn = {
+  '/': {
+    selectLanguageText: 'Languages',
+    editLinkText: 'Edit this page on GitHub',
+    contributorsText: 'Contributors',
+    lastUpdatedText: 'Last Updated',
+    notFound: ['Everything was found except the mind...'],
+    backToHome: 'Never forget where you come from',
+    navbar: [
+      {
+        text: 'Impress',
+        children: [
+          {
+            text: 'Overview',
+            link: '/impress/',
+            activeMatch: '^/impress/$',
+          },
+          {
+            text: 'Introduction',
+            link: '/impress/introduction.html',
+          },
+          {
+            text: 'Getting Started',
+            link: '/impress/getting-started.html',
+          },
+        ],
+      },
+      {
+        text: 'Stack',
+        children: [
+          {
+            text: 'Overview',
+            link: '/stack/',
+            activeMatch: '^/stack/$',
+          },
+          {
+            text: 'Metacom',
+            link: '/stack/metacom.html',
+          },
+          {
+            text: 'Metavm',
+            link: '/stack/metavm.html',
+          },
+          {
+            text: 'Metaschema',
+            link: '/stack/metaschema.html',
+          },
+          {
+            text: 'Metadomain',
+            link: '/stack/metadomain.html',
+          },
+          {
+            text: 'Metaconfiguration',
+            link: '/stack/metaconfiguration.html',
+          },
+          {
+            text: 'Metawatch',
+            link: '/stack/metawatch.html',
+          },
+          {
+            text: 'Metalog',
+            link: '/stack/metalog.html',
+          },
+          {
+            text: 'Metasql',
+            link: '/stack/metasql.html',
+          },
+          {
+            text: 'Metautil',
+            link: '/stack/metautil.html',
+          },
+          {
+            text: 'Metatests',
+            link: '/stack/metatests.html',
+          },
+          {
+            text: 'Concolor',
+            link: '/stack/concolor.html',
+          },
+          {
+            text: 'Common',
+            link: '/stack/common.html',
+          },
+        ],
+      },
+    ],
+  },
+};
